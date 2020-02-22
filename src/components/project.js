ec74d442507a4d80c1c2ad6cfd3b9c6145879473
Philip Hinch <pjhinch@gmail.com>
2020-02-22 11:48:05 -0800

    refactor in Project

--- src/components/project.js
+++ src/components/project.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { css, StyleSheet } from 'aphrodite';
 
 const styles = StyleSheet.create({
-  compact: {
+  Project: {
+    transition: 'all 0.6s ease',
+  },
+  compactProject: {
     minWidth: 320,
     width: '35%',
     height: '70vh',
@@ -30,8 +33,7 @@ const styles = StyleSheet.create({
     display: 'none',
   },
 
-  expandedOverlay: {
-    background: '#fcebe0',
+  expandedProject: {
     background: 'linear-gradient(180deg, #fcebe0 0%, #ddab7b 100%)',
     position: 'absolute',
     height: '100%',
@@ -70,81 +72,106 @@ const styles = StyleSheet.create({
   },
 });
 
+const Compact = ({ onExpand, ...props }) => {
+  const {
+    projectTitle,
+    projectSubtitle,
+    mainImage,
+    mainImageAlt,
+    innerImage,
+    innerImageAlt,
+  } = props;
+  return (
+    <div
+      key={`${projectTitle}-compact`}
+      onClick={onExpand}
+      className={css(styles.compact)}
+    >
+      <img
+        className={css(styles.mainImageCompact)}
+        src={mainImage}
+        alt={mainImageAlt}
+      />
+      <div className={css(styles.titleWrapperCompact)}>
+        <h1 className={css(styles.titleCompact)}>{projectTitle}</h1>
+        <h2 className={css(styles.subtitleCompact)}>{projectSubtitle}</h2>
+      </div>
+      <img
+        className={css(styles.innerImageCompact)}
+        src={innerImage}
+        alt={innerImageAlt}
+      />{' '}
+    </div>
+  );
+};
+
+const Expanded = ({ onCompact, ...props }) => {
+  const {
+    projectTitle,
+    projectSubtitle,
+    mainImage,
+    mainImageAlt,
+    innerImage,
+    innerImageAlt,
+    descriptionText,
+  } = props;
+  return (
+    <div className={css(styles.expandedOverlay)}>
+      <div
+        key={`${projectTitle}-expanded`}
+        className={css(styles.expandedContent)}
+      >
+        <div className={css(styles.titleWrapperExpanded)}>
+          <img
+            className={css(styles.mainImageExpanded)}
+            src={mainImage}
+            alt={mainImageAlt}
+          />
+          <h1 className={css(styles.titleExpanded)}>{projectTitle}</h1>
+          <h2 className={css(styles.subtitleExpanded)}>{projectSubtitle}</h2>
+        </div>
+        <p
+          className={css(styles.descriptionExpanded)}
+          dangerouslySetInnerHTML={{ __html: descriptionText }}
+        />
+      </div>
+      <button onClick={onCompact} className={css(styles.closeButton)}>
+        closee
+      </button>
+      <img
+        className={css(styles.innerImageExpanded)}
+        src={innerImage}
+        alt={innerImageAlt}
+      />
+    </div>
+  );
+};
+
 class Project extends React.Component {
   state = {
     expanded: false,
   };
 
   render() {
-    const {
-      projectTitle,
-      projectSubtitle,
-      mainImage,
-      mainImageAlt,
-      innerImage,
-      innerImageAlt,
-      descriptionText,
-    } = this.props;
     const { expanded } = this.state;
-
-    if (expanded) {
-      return (
-        <div className={css(styles.expandedOverlay)}>
-          <div
-            key={`${projectTitle}-expanded`}
-            className={css(styles.expandedContent)}
-          >
-            <div className={css(styles.titleWrapperExpanded)}>
-              <img
-                className={css(styles.mainImageExpanded)}
-                src={mainImage}
-                alt={mainImageAlt}
-              />
-              <h1 className={css(styles.titleExpanded)}>{projectTitle}</h1>
-              <h2 className={css(styles.subtitleExpanded)}>
-                {projectSubtitle}
-              </h2>
-            </div>
-            <p
-              className={css(styles.descriptionExpanded)}
-              dangerouslySetInnerHTML={{ __html: descriptionText }}
-            />
-          </div>
-          <button
-            onClick={() => this.setState({ expanded: false })}
-            className={css(styles.closeButton)}
-          >
-            closee
-          </button>
-          <img
-            className={css(styles.innerImageExpanded)}
-            src={innerImage}
-            alt={innerImageAlt}
-          />
-        </div>
-      );
-    }
-
     return (
       <div
-        key={`${projectTitle}-compact`}
-        onClick={() => this.setState({ expanded: true })}
-        className={css(styles.compact)}
+        className={css(
+          styles.Project,
+          expanded ? styles.expandedProject : styles.compactProject,
+        )}
       >
-        <img
-          className={css(styles.mainImageCompact)}
-          src={mainImage}
-          alt={mainImageAlt}
-        />
-        <div className={css(styles.titleWrapperCompact)}>
-          <h1 className={css(styles.titleCompact)}>{projectTitle}</h1>
-          <h2 className={css(styles.subtitleCompact)}>{projectSubtitle}</h2>
-        </div>
-        <img
-          className={css(styles.innerImageCompact)}
-          src={innerImage}
-          alt={innerImageAlt}
-        />{' '}
+        {expanded ? (
+          <Expanded
+            onCompact={() => this.setState({ expanded: false })}
+            {...this.props}
+          />
+        ) : (
+          <Compact
+            onExpand={() => this.setState({ expanded: true })}
+            {...this.props}
+          />
+        )}
       </div>
     );
   }
