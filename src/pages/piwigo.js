d800fb8bd40bf863112863e016bf891a29f0cbee
Philip Hinch <philip@philips-air.lan>
2021-12-30 17:03:55 -0800

    fix typo

--- src/pages/piwigo.js
+++ src/pages/piwigo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import PiwigoFetcher from '../components/piwigoFetcher';
+import PiwigoFetcher from '../components/PiwigoFetcher';
 
 import { AppScreen } from '../components/AppScreen';
 
